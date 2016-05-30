0e1c3df15c8d38995795bd93e2c1db7f00a5cbb5
erolanda <elmaildemlanda@gmail.com>
2016-05-29 19:00:02 -0500

    addded expressjs

--- nodejserver/index.js
+++ nodejserver/index.js
@@ -1,8 +1,26 @@
-var WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ port: 8080 });
+var server = require('http').createServer()
+  , url = require('url')
+  , WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({ server: server })
+  , express = require('express')
+  , app = express()
+  , port = 8080;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
   });
 });
+
+server.on('request', app);
+server.listen(port, function () { console.log('Listening on ' + server.address().port) });
