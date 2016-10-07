c8ba66fbeaaa79f4141fcff247004b38b05132b0
erolanda <elmaildemlanda@gmail.com>
2016-10-06 21:58:55 -0500

    minor changes

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-var server = require('http').createServer()
-  , url = require('url')
-  , WebSocketServer = require('ws').Server
-  , wss = new WebSocketServer({ server: server })
-  , express = require('express')
-  , app = express()
-  , router = express.Router()
-  , port = 3000
-  , path = require('path')
-  , os = require('os')
-  , CSV = require('comma-separated-values')
-  , fs = require('fs');
+var server = require('http').createServer(),
+    url = require('url'),
+    WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({
+        server: server
+    }),
+    express = require('express'),
+    app = express(),
+    router = express.Router(),
+    port = 3000,
+    path = require('path'),
+    os = require('os'),
+    CSV = require('comma-separated-values'),
+    fs = require('fs');
 
 //const fs = require('fs')
-const low = require('lowdb')
-const fileAsync = require('lowdb/lib/file-async')
+const low = require('lowdb');
+const fileAsync = require('lowdb/lib/file-async');
 
 // Start database using file-async storage
-const db = low()
+const db = low();
 
-db.defaults({ lectures: [] })
-  .value()
+db.defaults({
+        lectures: []
+    })
+    .value();
 
 // setup express middleware //
 app.use(router);
@@ -29,53 +33,67 @@ app.set('view engine', 'pug');
 
 // Router //
 router.use(function(req, res, next) {
-  // .. some logic here .. like any other middleware
-  next();
+    // .. some logic here .. like any other middleware
+    next();
 });
 
-router.get('/', function (req, res) {
-  res.render('index');
+router.get('/', function(req, res) {
+    res.render('index');
 });
 
-router.get('/view', function (req, res) {
-  res.render('view');
+router.get('/view', function(req, res) {
+    res.render('view');
 });
 
-router.get('/sendFile', function(req, res){
-  //processData();
-  console.log('send file');
-  res.download(__dirname + '/data.csv', 'data.csv');
+router.get('/sendFile', function(req, res) {
+    //processData();
+    console.log('send file');
+    res.download(__dirname + '/data.csv', 'data.csv');
 });
 
-function processData(){
-  const rawData = low('data.json', {
-    storage: fileAsync,
-    writeOnChange: false
-  })
-  rawData.defaults({ lectures: [] })
-    .value()
-  rawData.set('lectures', [])
-    .value()
-  db.get('lectures')
-    .forEach(function(value){
-        var parsed = JSON.parse(value.lectures);
-        for(i = 0; i < 5; i++){
-          rawData.get('lectures')
-            .push({cont: parsed.lectures[(8*i)+7], accX: parsed.lectures[(8*i)+0], accY: parsed.lectures[(8*i)+1], accZ: parsed.lectures[(8*i)+2], gyroX: parsed.lectures[(8*i)+3], gyroY: parsed.lectures[(8*i)+4], gyroZ: parsed.lectures[(8*i)+5], millis: parsed.lectures[(8*i)+6], id:parsed.ID})
-            .value()
+function processData() {
+    const rawData = low('data.json', {
+        storage: fileAsync,
+        writeOnChange: false
+    });
+    rawData.defaults({
+            lectures: []
+        })
+        .value();
+    rawData.set('lectures', [])
+        .value();
+    db.get('lectures')
+        .forEach(function(value) {
+            var parsed = JSON.parse(value.lectures);
+            for (i = 0; i < 5; i++) {
+                rawData.get('lectures')
+                    .push({
+                        cont: parsed.lectures[(8 * i) + 7],
+                        accX: parsed.lectures[(8 * i) + 0],
+                        accY: parsed.lectures[(8 * i) + 1],
+                        accZ: parsed.lectures[(8 * i) + 2],
+                        gyroX: parsed.lectures[(8 * i) + 3],
+                        gyroY: parsed.lectures[(8 * i) + 4],
+                        gyroZ: parsed.lectures[(8 * i) + 5],
+                        millis: parsed.lectures[(8 * i) + 6],
+                        id: parsed.ID
+                    })
+                    .value();
+            }
+        })
+        .value();
+    rawData.write();
+    jsonData = rawData.getState();
+    var csvData = new CSV(jsonData.lectures, {
+        header: true
+    }).encode();
+    console.log(csvData);
+    fs.writeFile('data.csv', csvData, function(err) {
+        if (err) {
+            return console.log(err);
         }
-      })
-    .value()
-  rawData.write();
-  jsonData = rawData.getState()
-  var csvData = new CSV(jsonData.lectures, {header: true}).encode();
-  console.log(csvData);
-  fs.writeFile('data.csv', csvData, function(err){
-    if(err){
-      return console.log(err);
-    }
-    console.log("the file was saved");
-  })
+        console.log("the file was saved");
+    });
 }
 
 //------ web sockets ------//
@@ -85,62 +103,64 @@ var sensors = [];
 var saving = false;
 
 wss.on('connection', function connection(ws) {
-  var location = url.parse(ws.upgradeReq.url, true);
-  console.log(ws.protocol);
-  if(ws.protocol == "client"){
-    console.log("agregar navegador");
-    clients.push(ws);
-  }else {
-    console.log("agregar esp8266");
-    sensors.push(ws);
-  }
-  // you might use location.query.access_token to authenticate or share sessions
-  // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
-
-  ws.on('message', function incoming(message) {
-    //console.log('received: %s', message);
-    if(message == "startPreview"){
-      sensors.forEach(function(sensor) {
-        sensor.send(message, function ack(error) {
-          // if error is not defined, the send has been completed,
-          // otherwise the error object will indicate what failed.
-        });
-      });
-    }else if(message == "stopPreview"){
-      sensors.forEach(function(sensor) {
-        sensor.send(message, function ack(error) {
-          // if error is not defined, the send has been completed,
-          // otherwise the error object will indicate what failed.
-        });
-      });
-    }else if(message == "calibrate"){
-      sensors.forEach(function(sensor) {
-        sensor.send(message, function ack(error) {
-          // if error is not defined, the send has been completed,
-          // otherwise the error object will indicate what failed.
-        });
-      });
-    }else if(message == "startSaving"){
-      saving = true;
-      db.set('lectures', [])
-        .value()
-    }else if(message == "stopSaving"){
-      saving = false;
-      processData();
-    }else{
-      clients.forEach(function(client){
-        client.send(message,function ack(error){
-          // if error is not defined, the send has been completed,
-          // otherwise the error object will indicate what failed.
-        });
-      });
-      if(saving){
-        db.get('lectures')
-          .push({lectures: message})
-          .value()
-      }
+    var location = url.parse(ws.upgradeReq.url, true);
+    console.log(ws.protocol);
+    if (ws.protocol == "client") {
+        console.log("agregar navegador");
+        clients.push(ws);
+    } else {
+        console.log("agregar esp8266");
+        sensors.push(ws);
     }
-  });
+    // you might use location.query.access_token to authenticate or share sessions
+    // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+    ws.on('message', function incoming(message) {
+        //console.log('received: %s', message);
+        if (message == "startPreview") {
+            sensors.forEach(function(sensor) {
+                sensor.send(message, function ack(error) {
+                    // if error is not defined, the send has been completed,
+                    // otherwise the error object will indicate what failed.
+                });
+            });
+        } else if (message == "stopPreview") {
+            sensors.forEach(function(sensor) {
+                sensor.send(message, function ack(error) {
+                    // if error is not defined, the send has been completed,
+                    // otherwise the error object will indicate what failed.
+                });
+            });
+        } else if (message == "calibrate") {
+            sensors.forEach(function(sensor) {
+                sensor.send(message, function ack(error) {
+                    // if error is not defined, the send has been completed,
+                    // otherwise the error object will indicate what failed.
+                });
+            });
+        } else if (message == "startSaving") {
+            saving = true;
+            db.set('lectures', [])
+                .value();
+        } else if (message == "stopSaving") {
+            saving = false;
+            processData();
+        } else {
+            clients.forEach(function(client) {
+                client.send(message, function ack(error) {
+                    // if error is not defined, the send has been completed,
+                    // otherwise the error object will indicate what failed.
+                });
+            });
+            if (saving) {
+                db.get('lectures')
+                    .push({
+                        lectures: message
+                    })
+                    .value();
+            }
+        }
+    });
 });
 
 //------ server declaration ------//
@@ -148,15 +168,15 @@ server.on('request', app);
 server.listen(port, rdy);
 
 function rdy() {
-  var interfaces = os.networkInterfaces();
-  var addresses = [];
-  for (var k in interfaces) {
-    for (var k2 in interfaces[k]) {
-      var address = interfaces[k][k2];
-      if (address.family === 'IPv4' && !address.internal) {
-        addresses.push(address.address);
-      }
+    var interfaces = os.networkInterfaces();
+    var addresses = [];
+    for (var k in interfaces) {
+        for (var k2 in interfaces[k]) {
+            var address = interfaces[k][k2];
+            if (address.family === 'IPv4' && !address.internal) {
+                addresses.push(address.address);
+            }
+        }
     }
-  }
-  console.log('Listening on ' + addresses[0] + ':' + server.address().port)
+    console.log('Listening on ' + addresses[0] + ':' + server.address().port);
 }
--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,193 +1,189 @@
 var banPreview = false;
 var banSave = false;
 var ws;
-var dps = new Array();
-var nSensors = 2;			// Number of sensors
+var dps = [];
+var nSensors = 2; // Number of sensors
 var nLecPackage = 5; // Number of lectures per JSON package
-var charts = new Array();
+var charts = [];
 var calibrated = 0;
 
-window.onload = function () {
-	document.getElementById("btnStartSaving").disabled = true;
-	//document.getElementById("btnExport").disabled = true;
-	//document.getElementById("btnExport").style.visibility = 'hidden';
-	for (i=0; i < nSensors; i++){
-		dps[i] = new Array();
-		for(j = 0; j < 8; j++){
-			dps[i][j] = new Array();
-		}
-	}
-	setupWebSocket();
-	setupPlots();
+window.onload = function() {
+    document.getElementById("btnStartSaving").disabled = true;
+    //document.getElementById("btnExport").disabled = true;
+    //document.getElementById("btnExport").style.visibility = 'hidden';
+    for (i = 0; i < nSensors; i++) {
+        dps[i] = [];
+        for (j = 0; j < 8; j++) {
+            dps[i][j] = [];
+        }
+    }
+    setupWebSocket();
+    setupPlots();
+};
+
+function setupWebSocket() {
+    var host = window.document.location.host.replace(/:.*/, '');
+    ws = new WebSocket('ws://' + host + ':3000', 'client');
+
+    // Log errors
+    ws.onerror = function(error) {
+        console.log('WebSocket Error ' + error);
+    };
+
+    // Log messages from the server
+    ws.onmessage = function(e) {
+        try {
+            var lectures = JSON.parse(e.data);
+            for (i = 0; i < nLecPackage; i++) {
+                for (j = 0; j < 8; j++) {
+                    var index = (8 * i) + j;
+                    dps[lectures.ID - 1][j].push({
+                        x: lectures.lectures[(8 * i) + 7],
+                        y: lectures.lectures[index]
+                    });
+                    console.log(lectures.lectures[(8 * i) + 6]);
+                }
+            }
+        } catch (e) {
+            calibrated += 1;
+            if (calibrated == nSensors) {
+                console.log("Calibration Complete");
+                NProgress.done();
+                swal.close();
+                swal(
+                    'Success',
+                    'Calibration complete!',
+                    'success'
+                );
+            }
+        }
+
+        // var sum = 0;
+        // for(i=0; i < dps[0][6].length - 1; i++){
+        // 	sum += (dps[0][6][i + 1].y) - (dps[0][6][i].y)
+        // }
+
+        var dataLength = 500; // number of dataPoints visible at any point - 10 seconds
+        if (dps[nSensors - 1][7].length == dataLength) {
+            for (i = 0; i < nSensors; i++) {
+                for (j = 0; j < 8; j++) {
+                    for (z = 0; z < nLecPackage; z++)
+                        dps[i][j].shift();
+                }
+            }
+        }
+        for (i = 0; i < nSensors; i++)
+            charts[i].render();
+    };
 }
 
-function setupWebSocket(){
-	var host = window.document.location.host.replace(/:.*/, '');
-	ws = new WebSocket('ws://' + host + ':3000', 'client');
-
-	// Log errors
-	ws.onerror = function (error) {
-  	console.log('WebSocket Error ' + error);
-	};
-
-	// Log messages from the server
-	ws.onmessage = function (e) {
-		try{
-			var lectures = JSON.parse(e.data);
-			for(i = 0; i < nLecPackage; i++){
-				for(j = 0; j < 8; j++){
-					var index = (8 * i)+j;
-					dps[lectures.ID - 1][j].push({
-						x: lectures.lectures[(8 * i)+7],
-						y: lectures.lectures[index]
-					})
-					console.log(lectures.lectures[(8 * i)+6]);
-				}
-			}
-		}catch(e){
-			calibrated += 1;
-			if(calibrated == nSensors){
-				console.log("Calibration Complete");
-				NProgress.done();
-				swal.close();
-				swal(
-				  'Success',
-				  'Calibration complete!',
-				  'success'
-				)
-			}
-		}
-
-		// var sum = 0;
-		// for(i=0; i < dps[0][6].length - 1; i++){
-		// 	sum += (dps[0][6][i + 1].y) - (dps[0][6][i].y)
-		// }
-
-		var dataLength = 500; // number of dataPoints visible at any point - 10 seconds
-		if(dps[nSensors - 1][7].length == dataLength){
-			for(i = 0; i < nSensors; i++){
-				for(j = 0; j < 8; j++){
-					for(z = 0; z < nLecPackage; z++)
-						dps[i][j].shift();
-				}
-			}
-		}
-		for(i = 0; i < nSensors; i++)
-			charts[i].render();
-	};
+function startPreview() {
+    banPreview = !banPreview;
+    if (banPreview) {
+        $("#btnStartPreview").html('Stop data');
+        document.getElementById("btnStartSaving").disabled = false;
+        ws.send("startPreview");
+    } else {
+        $("#btnStartPreview").html('Start data');
+        document.getElementById("btnStartSaving").disabled = true;
+        ws.send("stopPreview");
+    }
 }
 
-function startPreview(){
-	banPreview = !banPreview;
-	if (banPreview){
-		$("#btnStartPreview").html('Stop data');
-		document.getElementById("btnStartSaving").disabled = false;
-		ws.send("startPreview");
-	}else{
-		$("#btnStartPreview").html('Start data');
-		document.getElementById("btnStartSaving").disabled = true;
-		ws.send("stopPreview");
-	}
+function startSaving() {
+    banSave = !banSave;
+    if (banSave) {
+        $("#btnStartSaving").html('Stop saving data');
+        ws.send("startSaving");
+    } else {
+        $("#btnStartSaving").html('Start saving data');
+        ws.send("stopSaving");
+        startPreview();
+        document.getElementById("btnExport").disabled = false;
+        document.getElementById("btnExport").style.visibility = 'visible';
+        $("#btnStartPreview").html('Start data');
+    }
 }
 
-function startSaving(){
-	banSave = !banSave;
-	if(banSave){
-		$("#btnStartSaving").html('Stop saving data');
-		ws.send("startSaving");
-	}else{
-		$("#btnStartSaving").html('Start saving data');
-		ws.send("stopSaving");
-		startPreview();
-		document.getElementById("btnExport").disabled = false;
-		document.getElementById("btnExport").style.visibility = 'visible';
-		$("#btnStartPreview").html('Start data');
-	}
-}
-
-function calibrate(){
-	swal({
-	  title: 'Calibration',
-	  text: 'Your MPU6050 should be placed in horizontal position, with package letters facing up. Don\'t touch it until you see a finish message.',
-	  type: 'info',
-		allowOutsideClick: false,
-		allowEscapeKey: false,
-	  showCancelButton: true,
-		confirmButtonColor: '#1CA8DD',
-		cancelButtonColor: '#d33',
-	  confirmButtonText: 'Start calibration',
-	}).then(function() {
-		ws.send("calibrate")
-		NProgress.start();
-	  swal({
-			title: 'Calibration in progress',
-			text: 'Please wait. Don\'t touch the sensor until you see a finish message.',
-			type: 'info',
-			showConfirmButton: false,
-			allowOutsideClick: false,
-			allowEscapeKey: false
-		})
-	});
-}
-
-function download(){
-	window.open('/sendFile')
+function calibrate() {
+    swal({
+        title: 'Calibration',
+        text: 'Your MPU6050 should be placed in horizontal position, with package letters facing up. Don\'t touch it until you see a finish message.',
+        type: 'info',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        showCancelButton: true,
+        confirmButtonColor: '#1CA8DD',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Start calibration',
+    }).then(function() {
+        ws.send("calibrate");
+        NProgress.start();
+        swal({
+            title: 'Calibration in progress',
+            text: 'Please wait. Don\'t touch the sensor until you see a finish message.',
+            type: 'info',
+            showConfirmButton: false,
+            allowOutsideClick: false,
+            allowEscapeKey: false
+        });
+    });
 }
 
 function setupPlots() {
-	for(i = 0; i < nSensors; i++){
-		var name = "sensor".concat(i + 1);
-		charts[i] = new CanvasJS.Chart(name,{
-			backgroundColor: "transparent",
-			axisY:{
-				gridColor: "rgba(255,255,255,.05)",
-				tickColor: "rgba(255,255,255,.05)",
-				labelFontColor: "#a2a2a2"
-			},
-			axisX:{
-				labelFontColor: "#a2a2a2"
-			},
-			data: [{
-				type: "line",
-				showInLegend: true,
-				name: "accX",
-				dataPoints: dps[i][0]
-			},{
-				type: "line",
-				showInLegend: true,
-				name: "accY",
-				dataPoints: dps[i][1]
-			},{
-				type: "line",
-				showInLegend: true,
-				name: "accZ",
-				dataPoints: dps[i][2]
-			},{
-				type: "line",
-				showInLegend: true,
-				name: "gyroX",
-				dataPoints: dps[i][3]
-			},{
-				type: "line",
-				showInLegend: true,
-				name: "gyroY",
-				dataPoints: dps[i][4]
-			},{
-				type: "line",
-				showInLegend: true,
-				name: "gyroZ",
-				dataPoints: dps[i][5]
-			}],
-			legend: {
-				cursor: "pointer",
-				itemclick: function (e) {
-					if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible)
-						e.dataSeries.visible = false;
-					else
-						e.dataSeries.visible = true;
-					e.chart.render();
-				}
-			}
-		});
-	}
+    for (i = 0; i < nSensors; i++) {
+        var name = "sensor".concat(i + 1);
+        charts[i] = new CanvasJS.Chart(name, {
+            backgroundColor: "transparent",
+            axisY: {
+                gridColor: "rgba(255,255,255,.05)",
+                tickColor: "rgba(255,255,255,.05)",
+                labelFontColor: "#a2a2a2"
+            },
+            axisX: {
+                labelFontColor: "#a2a2a2"
+            },
+            data: [{
+                type: "line",
+                showInLegend: true,
+                name: "accX",
+                dataPoints: dps[i][0]
+            }, {
+                type: "line",
+                showInLegend: true,
+                name: "accY",
+                dataPoints: dps[i][1]
+            }, {
+                type: "line",
+                showInLegend: true,
+                name: "accZ",
+                dataPoints: dps[i][2]
+            }, {
+                type: "line",
+                showInLegend: true,
+                name: "gyroX",
+                dataPoints: dps[i][3]
+            }, {
+                type: "line",
+                showInLegend: true,
+                name: "gyroY",
+                dataPoints: dps[i][4]
+            }, {
+                type: "line",
+                showInLegend: true,
+                name: "gyroZ",
+                dataPoints: dps[i][5]
+            }],
+            legend: {
+                cursor: "pointer",
+                itemclick: function(e) {
+                    if (typeof(e.dataSeries.visible) === "undefined" || e.dataSeries.visible)
+                        e.dataSeries.visible = false;
+                    else
+                        e.dataSeries.visible = true;
+                    e.chart.render();
+                }
+            }
+        });
+    }
 }
